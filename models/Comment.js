3aca6bf083ea2049d9acdb2f10a53d769d22c8e3
rstanbaugh <rich_bootcamp@icloud.com>
2022-07-06 16:53:20 -0400

    comment and post routes

--- models/Comment.js
+++ models/Comment.js
@@ -13,7 +13,10 @@ Comment.init(
     },
     comment_text: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     },
     user_id: {
       type: DataTypes.INTEGER,
@@ -24,7 +27,6 @@ Comment.init(
     },
     post_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'post',
         key: 'id'
@@ -37,7 +39,6 @@ Comment.init(
     underscored: true,
     modelName: 'comment'
   }
-
 );
 
-module.exports =  Comment;
+module.exports = Comment;
