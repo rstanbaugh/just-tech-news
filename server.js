8b52ba4e1dd2fa4be878e1ea8bd49844c278050e
rstanbaugh <rich_bootcamp@icloud.com>
2022-07-10 12:15:57 -0400

    14.1.5 complete

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const path = require('path');
 const express = require('express');
-
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+ 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));  // serve public folder as static asset
 
 // Routes
-const routes = require('./routes');
+const routes = require('./controllers');
 app.use(routes); // turn on routes
 
 // turn on connection to db and server
